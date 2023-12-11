9108e4a3844b343bee90dfc62e3c6cd4753560cf
Evandro Lugli <evandro.lugli@gmail.com>
2023-12-11 23:02:53 +1100

    first commit

---
+++ src/screens/Home/index.tsx
@@ -0,0 +1,32 @@
+import { Text, TextInput, TouchableOpacity, View } from 'react-native';
+import { styles } from './styles';
+
+export default function Home(){
+    function handleParticipantAdd(){
+        console.log('clicou!!!')
+    }
+
+  return(
+    <View style={styles.container}>
+      <Text style={styles.eventName}>
+        Event Name
+      </Text>
+      <Text style={styles.eventDate}>
+        11 of December
+      </Text>
+      
+      <TextInput
+        style={styles.input}
+        placeholder="Name"
+        placeholderTextColor="#6B6B6B"
+        />
+
+        <TouchableOpacity style={styles.button} onPress={handleParticipantAdd}>
+            <Text style={styles.buttonText}>
+                +
+            </Text>
+        </TouchableOpacity>
+
+    </View>
+  )
+}
